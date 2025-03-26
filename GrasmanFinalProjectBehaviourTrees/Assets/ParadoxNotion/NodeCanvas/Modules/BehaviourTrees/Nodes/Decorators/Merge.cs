3f5b33e786134b48f4c214afee2d171107cc7891
Sophie_G <rosaulula@gmail.com>
2025-03-26 11:55:00 -0400

    Imported nodeCanvas

---
+++ GrasmanFinalProjectBehaviourTrees/Assets/ParadoxNotion/NodeCanvas/Modules/BehaviourTrees/Nodes/Decorators/Merge.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+
+namespace NodeCanvas.BehaviourTrees
+{
+
+    [Name("Merge", -1)]
+    [Description("Merge can accept multiple input connections and thus possible to re-use branches from multiple parents.")]
+    [Category("Decorators")]
+    [ParadoxNotion.Design.Icon("Merge")]
+    public class Merge : BTDecorator
+    {
+
+        public override int maxInConnections => -1;
+
+        protected override Status OnExecute(Component agent, IBlackboard blackboard) {
+            if ( status != Status.Running ) { decoratedConnection.Reset(); }
+            return decoratedConnection.Execute(agent, blackboard);
+        }
+    }
+}
