8ee149c4091df8398c7c1569e9216a61abb1a5a8
Sophie_G <rosaulula@gmail.com>
2025-03-28 12:43:02 -0400

    Ninja Teleport Motion Test

---
+++ GrasmanFinalProjectBehaviourTrees/Assets/Scripts/InRangeCT.cs
@@ -0,0 +1,39 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Conditions {
+
+	public class InRangeCT : ConditionTask {
+
+		public Transform target;
+		public float detectionRange;
+
+		//Use for initialization. This is called only once in the lifetime of the task.
+		//Return null if init was successfull. Return an error string otherwise
+		protected override string OnInit(){
+			return null;
+		}
+
+		//Called whenever the condition gets enabled.
+		protected override void OnEnable() {
+			
+		}
+
+		//Called whenever the condition gets disabled.
+		protected override void OnDisable() {
+			
+		}
+
+		//Called once per frame while the condition is active.
+		//Return whether the condition is success or failure.
+		protected override bool OnCheck() {
+			if (Vector3.Distance(agent.transform.position, target.position) <= detectionRange)
+			{
+                return true;
+            }
+			return false;
+		}
+	}
+}
